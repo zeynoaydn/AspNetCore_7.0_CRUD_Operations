893b0b3e9b9749ca67c9a4fc4a576d834b22a3dd
zeynoaydn <zeynoaydnn@gmail.com>
2023-08-18 10:02:49 +0300

    Add project files.

---
+++ Utility/ProjectDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mvc_Project.Models;
+
+namespace Mvc_Project.Utility
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {}
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
+        public DbSet<Kitap> Kitaplar { get; set; }
+    }
+}
